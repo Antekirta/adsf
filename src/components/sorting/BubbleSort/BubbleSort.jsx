4703ca8068a894eb8c77e8c06238fe6a05d9fc28
unknown <murashkin.kirill.konst@gmail.com>
2019-09-30 23:48:37 +0200

    Step-by-step smooth bubble sorting

--- src/components/sorting/BubbleSort/BubbleSort.jsx
+++ src/components/sorting/BubbleSort/BubbleSort.jsx
@@ -2,7 +2,10 @@ import React, { useState } from 'react'
 import "./BubbleSort.css"
 
 export const BubbleSort = () => {
-    let [arr, setArr] = useState([7, 8, 60, 4, 6, 6, 4, 435, 7, 32, 3, 74])
+    let [arr, setArr] = useState([7, 8, 60, 4, 6, 6, 4, 435, 7, 32, 3, 74, 345, 7, 46, 78, 132, 20, 98, 45, 37, 89, 4, 8, 3, 6, 5])
+
+    let [currentI, setCurrentI] = useState(null)
+    let [currentJ, setCurrentJ] = useState(null)
 
     return (
         <div className="bubble-sort">
@@ -10,8 +13,14 @@ export const BubbleSort = () => {
 
             <ul className="bubble-sort__list">
                 {arr.map((item, index) => {
+                    let className = 'bubble-sort__item'
+
+                    if ([currentI, currentJ].includes(index)) {
+                        className += ' active'
+                    }
+
                     return (
-                        <li className="bubble-sort__item" key={index}>{item}</li>
+                        <li className={className} key={index}>{item}</li>
                     )
                 })}
             </ul>
@@ -31,9 +40,11 @@ export const BubbleSort = () => {
 
                 setTimeout(() => {
                     if (initialArr[i] > initialArr[j]) {
-                        
                         [initialArr[i], initialArr[j]] = [initialArr[j], initialArr[i]] // swap 'em!
                     }
+
+                    setCurrentI(i)
+                    setCurrentJ(j)
 
                     // because otherwise useState wouldn't consider it as new array
                     setArr([...initialArr])
